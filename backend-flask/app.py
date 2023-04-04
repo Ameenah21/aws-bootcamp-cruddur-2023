8c741904aa277668da3486a0eed3f812c06595a6
Ameenah21 <aminatlawal21@gmail.com>
2023-04-04 17:11:26 +0000

    Cognito Setting up server side cognito

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.user_activities import *
@@ -74,8 +75,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -122,7 +123,11 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+  )
+  data = HomeActivities.run("AUTH HEADER")
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
