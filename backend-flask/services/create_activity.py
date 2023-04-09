651f8c2ef5b950d9faa33fe8f32e7f9d7ac9b293
Amina Lawal <62836196+Ameenah21@users.noreply.github.com>
2023-04-09 16:31:03 +0000

    Creating activities

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -66,3 +66,17 @@ class CreateActivity:
     )
     """
     #query_commit(sql)
+  def create_activity(handle, message, expires_at):
+    sql = db.template('activities','create')
+    uuid = db.query_commit(sql,{
+      'handle': handle,
+      'message': message,
+      'expires_at': expires_at
+    })
+    return uuid
+  
+  def query_object_activity(uuid):
+    sql = db.template('activities','object')
+    return db.query_object_json(sql,{
+      'uuid': uuid
+    })
