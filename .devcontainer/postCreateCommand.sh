6bd95dbe5c5b9efa01d08cd8ff96aac900fa18c1
Amina Lawal <62836196+Ameenah21@users.noreply.github.com>
2023-04-11 15:43:39 +0000

    Week 5: Rearranging DB Files and Creating DynamoDB Utility Scripts 1

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-# pm install frontend
+
+# npm install frontend
 cd /workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && npm i;
+
 # backend pip requirements
 cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt;
-# Postgresal
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb release -cs) -pgdg main" > /etc/apt/sources.list.d/pgdg.list';
-wet --quiet -0 - https://www.postaresal.orq/media/keys/ACCC4CF8.asc | sudo apt-key add -;
+
+# Postgresql
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list';
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
 sudo apt-get update -y;
-sudo apt install -y postgresql-client-13 libpq-dev
-#update ec2 security group
-aws ec2 modify-security-group-rules \
-    --group-id $DB_SG_ID \
-    --security-group-rules "SecurityGroupRuleId=$DB_SG_RULE_ID,SecurityGroupRule={Description=CODESPACES,IpProtocol=tcp,FromPort=5432,ToPort=5432,CidrIpv4=$CODESPACES_IP/32}"
+sudo apt install -y postgresql-client-13 libpq-dev;
+
+# RDS Update SG Rule
+export CODESPACES_IP="$(curl ifconfig.me)"
+cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./backend-flask/bin/update-sg-rule-codespaces
